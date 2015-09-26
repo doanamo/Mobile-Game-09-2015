2c258abf8303218b4cdbdeeb855baf8cbca4f417
Piotr Doan <doanpiotr@gmail.com>
2015-09-26 20:17:33 +0200

    Make desktop build mimic android environment.

--- desktop/src/com/gunstarpl/simplegame/desktop/DesktopLauncher.java
+++ desktop/src/com/gunstarpl/simplegame/desktop/DesktopLauncher.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gunstarpl.simplegame.Application;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher
+{
+	public static void main (String[] arg)
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 720;
+		config.height = 1280;
+
 		new LwjglApplication(new Application(), config);
 	}
 }
